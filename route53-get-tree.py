d1622dbf7756534790fa56de6af74c5df0ca1d14
Nick Schmalenberger <nicks@conviva.com>
2020-11-13 02:09:58 -0500

    update to use boto3, python3, support AAAA records

--- route53-get-tree.py
+++ route53-get-tree.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys, re, getopt, traceback
-from boto.route53.connection import Route53Connection
+import boto3
 import pygraphviz
 from colorama import init
 from colorama import Fore, Back, Style
@@ -13,16 +13,16 @@ depth=1
 maxdepth=20
 
 def usage():
-    print '''Walk a tree of CNAME and A records in a DNS zone on Amazon Route53.
+    print('''Walk a tree of CNAME and A or AAAA records in a DNS zone on Amazon Route53.
 
 Usage:
   route53-get-tree.py -h -q [ -g <graph filename> ] [ -d <max depth> ] -z <zone name> -n <name1>,...
-'''
+''')
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "qhz:n:g:d:")
 except getopt.GetoptError as err:
-    print str(err)
+    print(str(err))
     usage()
     sys.exit(1)
 
@@ -41,13 +41,6 @@ for o, a in opts:
     elif o == "-d":
         maxdepth=int(a)
         
-c = Route53Connection()
-zone=c.get_zone(ZONE)
-allrecords=zone.get_records()
-
-if graphfile:
-    thegraph=pygraphviz.AGraph(directed=True,rankdir='TB')
-
 def putadot(name):
     if not re.search('\.$',name):
         return name + "."
@@ -55,90 +48,96 @@ def putadot(name):
         return name
 
 def printrecord(record,my_depth,fgcolor):
+    values = [ rr["Value"] for rr in record["ResourceRecords"] ]
     whitespace=""
     for tab in range(1,my_depth):
         whitespace+="\t"
-    string="{}{}↳ {} {}".format(whitespace,fgcolor,record.type,', '.join(record.resource_records))
-    if record.region:
-        string+=" {}[LBR:{}]{}".format(Fore.MAGENTA,record.region,fgcolor)
-    elif record.weight:
-        if int(record.weight)==0:
-            string+=" {}{}[WRR:{}]{}{}".format(Fore.BLACK,Back.RED,record.weight,fgcolor,Back.RESET)
+    string="{}{}↳ {} {}".format(whitespace,fgcolor,record["Type"],', '.join(values))
+    if record.get("Region") is not None:
+        string+=" {}[LBR:{}]{}".format(Fore.MAGENTA,record["Region"],fgcolor)
+    elif record.get("Weight") is not None:
+        if int(record["Weight"])==0:
+            string+=" {}{}[WRR:{}]{}{}".format(Fore.BLACK,Back.RED,record["Weight"],fgcolor,Back.RESET)
         else:
-            string+=" {}[WRR:{}]{}".format(Fore.RED,record.weight,fgcolor)
-    if record.identifier:
-        string+=" ({})".format(record.identifier)
-    if record.type == 'CNAME' and not record.resource_records[0].endswith('.'):
+            string+=" {}[WRR:{}]{}".format(Fore.RED,record["Weight"],fgcolor)
+    if record.get("SetIdentifier") is not None:
+        string+=" ({})".format(record["SetIdentifier"])
+    if record["Type"] == 'CNAME' and not values[0].endswith('.'):
         string+=" {}{}Warning no . at end of CNAME right hand side.{}{}".format(Fore.BLACK,Back.RED,fgcolor,Back.RESET)
-    print string
+    print(string)
 
 def graphrecord(record,nodecolor):
     global thegraph
 
+    values = [ rr["Value"] for rr in record["ResourceRecords"] ]
     nodeidentifier=""
     edgelabel=""
     edgefontcolor=""
-    if record.identifier:
-        nodeidentifier+="\n({})".format(record.identifier)
-    if record.region:
-        edgelabel="{}".format(record.region)
+    if record.get("Region") is not None:
+        edgelabel="{}".format(record["Region"])
         edgefontcolor="magenta"
-    elif record.weight:
-        if int(record.weight)==0:
+    elif record.get("Weight") is not None:
+        if int(record["Weight"])==0:
             edgefontcolor="red"
-        edgelabel="{}".format(record.weight)
+        edgelabel="{}".format(record["Weight"])
+    if record.get("SetIdentifier") is not None:
+        nodeidentifier+="\n({})".format(record["SetIdentifier"])
 
-    if record.type == 'CNAME':
-        nodename=putadot(record.resource_records[0])
-        thegraph.add_node(nodename,label="{} {}{}".format(record.type,nodename,nodeidentifier),color=nodecolor,style='filled')
-        thegraph.add_edge(record.name,nodename,label=edgelabel,fontcolor=edgefontcolor)
-    elif record.type == 'A':
-        nodename=",".join(record.resource_records)
-        thegraph.add_node(nodename,label="{} {}{}".format(record.type,nodename,nodeidentifier),color=nodecolor,style='filled')
-        thegraph.add_edge(record.name,nodename,label=edgelabel,fontcolor=edgefontcolor)
+    if record["Type"] == 'CNAME':
+        nodename=putadot(values[0])
+        thegraph.add_node(nodename,label="{} {}{}".format(record["Type"],nodename,nodeidentifier),color=nodecolor,style='filled')
+        thegraph.add_edge(record["Name"],nodename,label=edgelabel,fontcolor=edgefontcolor)
+    elif record["Type"] == 'A' or record["Type"] == 'AAAA':
+        nodename=",".join(values)
+        thegraph.add_node(nodename,label="{} {}{}".format(record["Type"],nodename,nodeidentifier),color=nodecolor,style='filled')
+        thegraph.add_edge(record["Name"],nodename,label=edgelabel,fontcolor=edgefontcolor)
 
-def recurseCNAMEsorArecords(my_zone,my_allrecords,my_NAME,my_depth):
+def recurseCNAMEsorAddressRecords(my_zone,my_allrecords,my_NAME,my_depth):
     global quiet
     global graphfile
     if graphfile:
         global thegraph
     CNAMErecords=list()
     Arecords=list()
+    AAAArecords=list()
 
     my_NAME=putadot(my_NAME)
     for record in my_allrecords:
-        if record.name==my_NAME and record.type=='CNAME':
+        if record["Name"]==my_NAME and record["Type"]=='CNAME':
             CNAMErecords.append(record)
-        elif record.name==my_NAME and record.type=='A':
+        elif record["Name"]==my_NAME and record["Type"]=='A':
             Arecords.append(record)
+        elif record["Name"]==my_NAME and record["Type"]=='AAAA':
+            AAAArecords.append(record)
 
-    if CNAMErecords and Arecords:
-        print "Thats weird, both CNAME and A records came back for the name. The route53 control panel doesn't allow this:"
-        print CNAMErecords
-        print Arecords
+    if CNAMErecords and (Arecords or AAAArecords):
+        print("Thats weird, both CNAME and A or AAAA records came back for the name. The route53 control panel doesn't allow this:")
+        print(CNAMErecords)
+        print(Arecords)
         return False
     elif CNAMErecords:
         for record in CNAMErecords:
             # if we hit max depth, don't recurse anymore
             # if the value of this record is not in our zone, don't recurse anymore
             # the right hand side of the CNAME should end with a . or this will have trouble
-            if my_depth<maxdepth and putadot(record.resource_records[0]).endswith(my_zone.name):
+            CNAMEvalue=record["ResourceRecords"][0]["Value"]
+            if my_depth<maxdepth and putadot(CNAMEvalue).endswith(my_zone["Name"]):
                 if graphfile:
                     graphrecord(record,'cyan')
                 if not quiet:
                     printrecord(record,my_depth,Fore.CYAN)
                 try:
-                    recurseCNAMEsorArecords(my_zone,my_allrecords,record.resource_records[0],my_depth+1)
+                    recurseCNAMEsorAddressRecords(my_zone,my_allrecords,CNAMEvalue,my_depth+1)
                 except:
-                    print "could not recurse {}".format(record.resource_records)
+                    print("could not recurse {}".format(record["ResourceRecords"]))
                     traceback.print_exc()
             else:
                 if graphfile:
                     graphrecord(record,'chartreuse')
                 if not quiet:
                     printrecord(record,my_depth,Fore.GREEN)
-    elif Arecords:
-        for record in Arecords:
+    elif Arecords or AAAArecords:
+        for record in Arecords + AAAArecords:
             if graphfile:
                 graphrecord(record,'chartreuse')
             if not quiet:
@@ -146,13 +145,26 @@ def recurseCNAMEsorArecords(my_zone,my_allrecords,my_NAME,my_depth):
     else:
         return False
 
+ZONE = putadot(ZONE)
+client = boto3.client('route53')
+zone = [zone for zone in client.list_hosted_zones()["HostedZones"] if zone["Name"] == ZONE][0]
+
+allrecords=list()
+paginator = client.get_paginator('list_resource_record_sets')
+page_iterator = paginator.paginate(HostedZoneId=zone["Id"])
+for page in page_iterator:
+    allrecords += page["ResourceRecordSets"]
+
+if graphfile:
+    thegraph=pygraphviz.AGraph(directed=True,rankdir='TB')
+
 for name in NAMES:
     name=putadot(name)
     if not quiet:
-        print name # the root name
+        print(name) # the root name
     if graphfile:
         thegraph.add_node(name)
-    recurseCNAMEsorArecords(zone,allrecords,name,depth)
+    recurseCNAMEsorAddressRecords(zone,allrecords,name,depth)
 
 if graphfile:
     thegraph.draw(graphfile,prog='dot')
